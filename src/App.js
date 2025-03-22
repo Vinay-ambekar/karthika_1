426945a8e552ce32b6c30d572f8dc09a98d43458
vinay <vinayambekar.b@gmail.com>
2025-03-22 21:39:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+// import './assets/lib/mobile-nav/mobile-nav.js';
+
+function App() {
+  
+  return (
+  <>
+  <Home/>
+  </>
+  );
+}
+
+export default App;
